7b78e6be1089206065b94d7d2fbfb688df888a1a
AlejandroPinaMartinez <146063896+AlejandroPinaMartinez@users.noreply.github.com>
2024-09-19 18:44:04 +0200

    Add files via upload

--- Rot13.java
+++ Rot13.java
@@ -1,33 +1,73 @@
 import java.util.Scanner;
 
 public class Rot13 {
-    
+    public static final String abcP = "abcdefghijklmnopqrstuvwxyz";
+    public static final String abcG = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Inserte la contraseña:");
         String contraseña = sc.nextLine();
-        String abcP = "abcdefghijklmnopqrstuvwxyz";
-        String abcG = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String xifrada = xifraRot13(contraseña, abcP, abcG);
-        System.out.println(xifrada);
+        if (contraseña.isEmpty()){System.out.println("Contraseña no vàlida");} 
+        else {
+        String xifrada = xifraRot13(contraseña);
+        System.out.println("Contraseña xifrada :" + xifrada);
+        String desxifrada = desxifraRot13(xifrada);
+        System.out.println("Contraseña desxifrada :" + desxifrada);
+        }
     }
 
-    public static String xifraRot13 (String contraseña, String abcP, String abcG) {
+    public static String xifraRot13 (String contraseña) {
     String xifrat= "";
         for (int i=0; i < contraseña.length(); i++) {
-            for (int j=0; j < abcG.length(); j++) {
-                    if (abcG.charAt(j) == contraseña.charAt(i) ) {
-                        xifrat=xifrat + abcG.charAt(j+13);
-                    }
-                    else if (abcP.charAt(j) == contraseña.charAt(i) ) {
-                        xifrat=xifrat + abcP.charAt(j+13);
-                    }
+            if (Character.isLetter(contraseña.charAt(i))) {
+                for (int j=0; j < 26; j++) {
+                        if (abcG.charAt(j) == contraseña.charAt(i) ) {
+                            if (j+13<=26){
+                                xifrat=xifrat + abcG.charAt(j+13);
+                            } else {
+                                xifrat=xifrat + (abcG.charAt((j+13)-26));
+                            }
+                        }
+                        else if (abcP.charAt(j) == contraseña.charAt(i) ) {
+                            if (j+13<=26){
+                                xifrat=xifrat + abcP.charAt(j+13);
+                            } else {
+                                xifrat=xifrat + (abcP.charAt((j+13)-26));
+                            }
+                        } 
+                }
+            } else {
+                xifrat = xifrat + contraseña.charAt(i);
             }
         }
         return xifrat;
     }
 
-    //public String desxifraRot13{
+    public static String desxifraRot13 (String xifrada){
+        String desxifrar= "";
+        for (int i=0; i < xifrada.length(); i++) {
+            if (Character.isLetter(xifrada.charAt(i))) {
+                for (int j=0; j < 26; j++) {
+                        if (abcG.charAt(j) == xifrada.charAt(i) ) {
+                            if (j-13>=0){
+                                desxifrar=desxifrar + abcG.charAt(j-13);
+                            } else {
+                                desxifrar=desxifrar + (abcG.charAt((j-13)+26));
+                            }
+                        }
+                        else if (abcP.charAt(j) == xifrada.charAt(i) ) {
+                            if (j-13>=0){
+                                desxifrar=desxifrar + abcP.charAt(j-13);
+                            } else {
+                                desxifrar=desxifrar + (abcP.charAt((j-13)+26));
+                            }
+                        } 
+                }
+            } else {
+                desxifrar = desxifrar + xifrada.charAt(i);
+            }
+        }
+        return desxifrar;
         
-    //}
+    }
 }
