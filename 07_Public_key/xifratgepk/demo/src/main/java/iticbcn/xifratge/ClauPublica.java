2842920304e5aa3ebce78658fe3659b5366033fb
alep <alpimarti10@gmail.com>
2024-11-06 19:58:18 +0100

    clase creada

---
+++ 07_Public_key/xifratgepk/demo/src/main/java/iticbcn/xifratge/ClauPublica.java
@@ -0,0 +1,21 @@
+package iticbcn.xifratge;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class ClauPublica {
+
+    public KeyPair generaParellClausRSA() throws Exception {
+
+    }
+    
+    public byte[] xifraRSA(String msg, PublicKey clauPublica)throws Exception {
+
+    }
+
+    public String desxifraRSA(byte[] msgXifrat, PrivateKey ClauPrivada) {
+
+    }
+
+}
